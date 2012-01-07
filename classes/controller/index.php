6ae12fb7301c382306346eb85cf41cda6922e547
Kemal Delalic <kemal.delalic@gmail.com>
2012-01-07 14:35:21 +0100

    removed ambiguous comment in Controller_Index::action_index()

--- classes/controller/index.php
+++ classes/controller/index.php
@@ -4,13 +4,7 @@ class Controller_Index extends Controller_Cachable {
 
 	public function action_index()
 	{
-		/*
-		foreach (ORM::factory('post')->find_all() as $post)
-		{
-			$post->text = $post->text.' ';
-			$post->update();
-		}
-		*/
+		
 	}
 
 }
